d1455eb70e85e18bfe45acd6dd32a8da33d9e400
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-07-29 23:10:17 +0100

    create index.php the home view helpers php that has the path as the parameter for the __DIR__

---
+++ helpers.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Get Base Path
+ * 
+ * @param string $path
+ * @return string
+ */
+
+ function basePath($path = ''){
+    return __DIR__ . '/' . $path; 
+ }
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+require '../helpers.php';
+
+// require the function from helpers and pass the @param
+require basePath('/views/home.view.php');
+
+
+echo 'hello world';
