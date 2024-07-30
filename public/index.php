aff8a4b4ee34dbd1583e74b2af32f6252134221f
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-07-30 15:28:57 +0100

    create .htaccess
    and on the index.php
    create a $route array
    and use the $_SERVER superglobal to request URI
    Using the inspectAndDie helper function it shows the $uri variable

--- public/index.php
+++ public/index.php
@@ -2,6 +2,20 @@
 require '../helpers.php';
 
 // require the function from helpers and pass the @param
-loadView('home');
+// loadView('home');
 
+
+// create array
+$routes = [
+    '/' => 'controllers/home.php',
+    '/listing' => 'controllers/listings/index.php',
+    '/listing/create' => 'controllers/listing/create.php',
+    '404' => 'controllers/error/404.php',
+];
+
+// router logic
+// current URI with $_SERVER super global
+
+$uri = $_SERVER['REQUEST_URI'];
+inspectAndDie($uri);
 
