3dd69f410847a7f2235c556a1308b6b954454854
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-07-31 23:12:24 +0100

    update

--- controllers/home.php
+++ controllers/home.php
@@ -4,8 +4,8 @@ $config = require '../config/db.php';
 $db = new Database($config);
 
 $listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
-$employee = $db->query('SELECT * FROM employee LIMIT 6')->fetchAll();
+$users = $db->query('SELECT * FROM users LIMIT 6')->fetchAll();
 
-inspect($employee);
+inspect($users);
 
 loadView('home');
