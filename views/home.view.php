d8b51f15faa84aa50e6fc918948d376f8f60f7f7
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-01 11:04:06 +0100

    on Database.php changed the PDO::FETCH_ASSOC into PDO::FETCH_OBJ
    then on view use the syntax
    
    <?= $listing->title ?>
    
    instead of

--- views/home.view.php
+++ views/home.view.php
@@ -24,19 +24,19 @@
               <?= $listing->description ?>
               </p>
               <ul class="my-4 bg-gray-100 p-4 rounded">
-                <li class="mb-2"><strong>Salary:</strong> <?= $listing['salary'] ?></li>
+                <li class="mb-2"><strong>Salary:</strong> <?= $listing->salary?></li>
                 <li class="mb-2">
-                  <strong>Location:</strong>  <?= $listing['city']?>,  <?= $listing['state'] ?>
+                  <strong>Location:</strong>  <?= $listing->city?>,  <?= $listing->state ?>
                   <!-- <span
                     class="text-xs bg-blue-500 text-white rounded-full px-2 py-1 ml-2"
                     >Local</span
                   > -->
                 </li>
                 <li class="mb-2">
-                  <strong>Tags:</strong>  <?= $listing['tags'] ?>
+                  <strong>Tags:</strong>  <?= $listing->tags ?>
                 </li>
               </ul>
-              <a href="/listings/<?= $listing['id'] ?>"
+              <a href="/listings/<?= $listing->id ?>"
                 class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200"
               >
                 Details
