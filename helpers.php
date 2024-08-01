86008be09a568615faf1629743319833bec6641b
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-01 14:31:46 +0100

    Refactors and update paths

--- helpers.php
+++ helpers.php
@@ -20,7 +20,7 @@
 
   // $data equals an empty array 
   function loadView($name, $data = [] ){
-   $viewPath = basePath("views/{$name}.view.php");
+   $viewPath = basePath("App/views/{$name}.view.php");
 
    // the inspect() from bottom which gives the var_dump() value
    // inspect($viewPath);
@@ -43,7 +43,7 @@
   */
 
   function loadPartial($name){
-   $partialPath =  basePath("views/partials/{$name}.php");
+   $partialPath =  basePath("App/views/partials/{$name}.php");
 
    if(file_exists($partialPath)){
       require $partialPath;
